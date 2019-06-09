018e1ed3cbc289196d26f5adfa5a3b9d3a74a80b
kshitij23 <kshitij.mehta23@gmail.com>
2019-06-09 12:23:12 -0700

    Final Devops01.sh

--- devops01a.sh
+++ devops01a.sh
@@ -1,2 +1,20 @@
-#!/bin/bash 
-#testnewbranch
+#!/bin/bash
+#Gets information about your computer including:
+#number of volumes, size of each volume, free space on each volume
+#number cpus/cores, information about the cpus/core
+#amount of ram, your mac address and ip addresa
+echo "================================================\n"
+echo "Disk Stats\n"
+echo "================================================\n"
+df -h
+echo "================================================\n"
+echo "CPU & Cores Stats\n"
+echo "================================================\n"
+lscpu 
+echo "================================================\n"
+echo "RAM Stats\n"
+echo "================================================\n"
+cat /proc/meminfo
+echo "================================================\n"
+ifconfig -all
+echo "================================================\n" 
